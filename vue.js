eefbd03b759df053c904a36ffbf97b59b556d9a1
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-20 16:21:11 +0100

    modifiche js

--- vue.js
+++ vue.js
@@ -18,24 +18,15 @@ var app = new Vue({
     },
 
     mounted() {
-      const self = this;
       axios
         .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(function(risposta) {
-          console.log(risposta);
-          console.log(risposta.data.response);
-          // self.mailList.mail1 = risposta.data.response;
-          self.mailList.mail1 = risposta.data.response
-          self.mailList.mail2 = risposta.data.response
-          self.mailList.mail3 = risposta.data.response
-          let mailList = self.mailList
-          let mail = risposta.data.response
-          console.log(mail);
-          for (var element in mailList) {
-            element = mail
-            console.log(element);
-          }
-        });
+        .then((risposta) => {
+          this.mailList.mail1 = risposta.data.response
 
+        }).then((risposta) => {
+          console.log(risposta);
+          
+
+        });
     }
 });
